aca3049e7ec03fe7b411a72bef22d361a5ef31ac
Matheus Cavalcante <jmatheusoc269@gmail.com>
2021-04-01 14:13:58 -0300

    adicionado capa dos filmes

---
+++ project.js
@@ -0,0 +1,8 @@
+var lfilmes = ['https://m.media-amazon.com/images/M/MV5BMDU2ZWJlMjktMTRhMy00ZTA5LWEzNDgtYmNmZTEwZTViZWJkXkEyXkFqcGdeQXVyNDQ2OTk4MzI@._V1_UX182_CR0,0,182,268_AL_.jpg',
+            'https://m.media-amazon.com/images/M/MV5BMTk3NDE2NzI4NF5BMl5BanBnXkFtZTgwNzE1MzEyMTE@._V1_UX182_CR0,0,182,268_AL_.jpg', 
+            'https://m.media-amazon.com/images/M/MV5BMTY1NTI0ODUyOF5BMl5BanBnXkFtZTgwNTEyNjQ0MDE@._V1_UX182_CR0,0,182,268_AL_.jpg' ]
+
+
+for (var i=0; i < lfilmes.length; i++) {
+    document.write("<img src =" + lfilmes[i] + ">")
+}
